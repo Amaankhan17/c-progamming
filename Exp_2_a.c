5915e61f3a9cab6220163dbddae74a48ea192638
Amaankhan17 <amaankhan134@eng.rizvi.edu.in>
2025-11-30 20:21:20 +0530

    Add files via upload

---
+++ Exp_2_a.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    Name: Amaan Khan
+    UIN: 251P045
+    Roll No: 41
+    Division: C
+    Class: Comps
+    */
+
+    int num1;
+    printf("Enter the Number: \n");
+    scanf("%d", &num1);
+    if (num1 % 2 == 0)
+    {
+        printf("It is an Even Number");
+    }
+    else
+    {
+        printf("It is an Odd Number");
+    }
+    return 0;
+}
