522ef5fab2693993cc07952b49b5db2f4cb6bed9
Amaankhan17 <amaankhan134@eng.rizvi.edu.in>
2025-11-30 20:14:47 +0530

    Add files via upload

---
+++ Exp_1_a.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    Name: Amaan Khan
+    UIN: 251P045
+    Roll No: 41
+    Division: C
+    Class: Comps
+    */
+
+    printf("Name: Amaan Khan \n");
+    printf("Age: 18 Years \n");
+    printf("Class: Computer Science  \n");
+    printf("UIN: 251P045");
+
+    return 0;
+}
